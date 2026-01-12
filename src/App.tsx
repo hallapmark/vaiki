eed79fb5ed49af77894d01cc0da82bafe57a49f6
Mark Hallap <hallapmark@gmail.com>
2026-01-12 20:12:20 +0200

    Add MovieListPage as the root element

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
-import './App.css'
-import {HlsPlayer} from "./components/HlsPlayer.tsx";
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import { MovieListPage } from "./pages/MovieListPage.tsx";
 
 function App() {
-
   return (
-      <div className="min-h-screen bg-neutral-950 flex flex-col items-center justify-center p-4 md:p-8">
-          <div className="w-full max-w-[min(1400px,142vh)] aspect-video max-h-[80vh]">
-              <HlsPlayer
-              src="sourcegoeshere"
-              className="w-full h-full shadow-2xl"
-              />
-          </div>
-      </div>
-  )
+    <div>
+      <Routes>
+        <Route path="/" element={<MovieListPage />} />
+      </Routes>
+      
+    </div>
+  );
 }
 
-export default App
+export default App;

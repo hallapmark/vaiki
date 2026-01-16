da89cf0bb7451b64ab236cf3cf74833249eae09d
Mark Hallap <hallapmark@gmail.com>
2026-01-16 12:28:28 +0200

    Make the header non-sticky

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import { Film } from "lucide-react";
 
 export function Header() {
   return (
-    <header className="sticky top-0 z-50">
+    <header className="top-0 z-50">
       <div className="mx-auto max-w-7xl px-6 lg:px-8 flex h-16 items-center">
         {/* Logo */}
         <Link
